4573dfc0e71739c931cdc6659c165d05329f5352
Luiz Eduardo Cardozo Pereira <eduardo.cardozo@olist.com>
2022-03-19 17:30:44 -0300

    chore: add search bar to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,9 @@
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Grid, Toolbar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import { MenuAccount } from "./Menus/MenuAccount";
 import { MenuCategories } from "./Menus/MenuCategories";
 import { Notifications } from './Notifications'
+import { InputSearch } from "./InputSearch/InputSearch";
 import Title from './Title';
 
 const useStyles = makeStyles((themes) => ({
@@ -19,6 +20,11 @@ const Navbar: React.FunctionComponent = () => {
                 <Toolbar>
                     <MenuCategories />
                     <Title />
+                    <Grid container justifyContent="center">
+                        <Grid item xs={8} lg={7}>
+                            <InputSearch />
+                        </Grid>
+                    </Grid>
                     <Notifications />
                     <MenuAccount />
                 </Toolbar>
