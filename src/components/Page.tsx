06040307cd7b99768cbe31549ff2b3aec5dc2e4c
Luiz Eduardo Cardozo Pereira <eduardo.cardozo@olist.com>
2022-03-19 23:18:55 -0300

    chore: adds page base component

---
+++ src/components/Page.tsx
@@ -0,0 +1,43 @@
+import { makeStyles } from "@material-ui/core";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme) => ({
+    root: (props: PageProps) => ({
+        height: 'inherit',
+        paddingTop: props.margin == "normal" ? 48 : 62,
+
+        [theme.breakpoints.between("sm", "md")]: {
+            paddingTop: props.margin == "normal" ? 48 : 62,
+        },
+
+        [theme.breakpoints.up("md")]: {
+            paddingTop: props.margin == "normal" ? 62 : 80,
+        },
+
+        '&::after': {
+            [theme.breakpoints.down(theme.breakpoints.values.mobile)]: {
+                display: "block",
+                content: "",
+                paddingTop: 100,
+            },
+        },
+    }),
+}));
+
+interface PageProps {
+    classes?: {
+        root?: any
+    };
+    margin?: "normal" | "large";
+}
+
+export const Page: React.FC<PageProps> = (props) => {
+    const { classes: propClasses = {} } = props;
+    const classes = useStyles(props);
+    const classRoot = propClasses.root ? clsx(classes.root, propClasses.root) : classes.root;
+    return (
+        <div className={classRoot}>
+            {props.children}
+        </div>
+    );
+}
